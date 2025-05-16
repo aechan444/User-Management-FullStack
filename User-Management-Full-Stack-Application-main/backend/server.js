2b9e82193509e9423a46630bb6f93a019314d75a
Chan <firefoxd92@gmail.com>
2025-05-16 23:12:20 +0800

    server.js fixing

--- User-Management-Full-Stack-Application-main/backend/server.js
+++ User-Management-Full-Stack-Application-main/backend/server.js
@@ -24,7 +24,7 @@ const allowedOrigins = [
     'https://user-management-full-stack-application-frontend.onrender.com',
     'https://user-management-full-stack-application-zeta.vercel.app',
     'https://user-management-full-stack-application.vercel.app',
-    'https://user-management-full-stack-8kpn-i3o38mooc.vercel.app' // ✅ ADD THIS
+    'https://user-management-full-stack-8kpn-6q59mcdys.vercel.app' // ✅ ADD THIS
 ];
 
 // Parse JSON and URL-encoded data
